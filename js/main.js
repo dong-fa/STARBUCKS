8cb054698a9ba74ff0103853d4a644c068dc03c8
dong-fa <120082854+dong-fa@users.noreply.github.com>
2024-01-18 18:42:49 +0900

    badges
    사이드 뱃지 제작
    
    lodash 라이브러리 추가
    gsap 라이브러리 추가
    
    스크롤시 뱃지가 사라지는 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -15,3 +15,22 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector(".badges");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
